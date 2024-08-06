66f7ce7d7c80b8009ec1c5cb26411a6dbb7ddf95
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2024-08-06 08:42:33 -0400

    site

---
+++ script.js
@@ -0,0 +1,7 @@
+const names = ["Steve", "Fang", "Mayur", "Billy", "Diane W", "Joe", "Casey", "Chris", "Caitlin", "Cindy", "Diane K", "Tom", "Jorge", "Ray"];
+
+document.getElementById('pickNameButton').addEventListener('click', () => {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const selectedName = names[randomIndex];
+    document.getElementById('result').textContent = `Chosen Name: ${selectedName}`;
+});
