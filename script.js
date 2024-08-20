df7043296c272744fee3c66cc72c8c28795c7866
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2024-08-20 10:13:56 -0400

    Update script.js
    Allow for no one to have to present the following week

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const names = ["Steve", "Fang", "Mayur", "Billy", "Diane W", "Joe", "Casey", "Chris", "Caitlin", "Cindy", "Diane K", "Tom", "Jorge", "Ray"];
+const names = ["Steve", "Fang", "Mayur", "Billy", "Diane W", "Joe", "Casey", "Chris", "Caitlin", "Cindy", "Diane K", "Tom", "Jorge", "Ray", "Off the Hook"];
 
 document.getElementById('pickNameButton').addEventListener('click', () => {
     const randomIndex = Math.floor(Math.random() * names.length);
     const selectedName = names[randomIndex];
-    document.getElementById('result').textContent = `Who's next? It's ${selectedName}! Get ready ${selectedName} we look forward to it :)`;
+    
+    if (selectedName === "Off the Hook") {
+        document.getElementById('result').textContent = "Nobody has to present next week! You're all off the hook!";
+    } else {
+        document.getElementById('result').textContent = `Who's next? It's ${selectedName}! Get ready ${selectedName} we look forward to it :)`;
+    }
 });
